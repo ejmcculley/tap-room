8bf8f3258e972f1947846262a3b60132c131fd24
ejmcculley <92233564+ejmcculley@users.noreply.github.com>
2022-06-04 19:11:01 -0700

    Add Header and TapRoomManager components to App.js

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from "./Header";
+import TapRoomManager from "./TapRoomManager";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <TapRoomManager />
+    </>
+  );
+}
+
+export default App;
