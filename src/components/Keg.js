bd8a7284039b2285731beffa010134acb6ded85a
ejmcculley <92233564+ejmcculley@users.noreply.github.com>
2022-06-10 13:50:38 -0700

    Add useState and decrementCount to keg.js

--- src/components/Keg.js
+++ src/components/Keg.js
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
 function Keg(props) {
+
+  const [state, setState] = useState({ count: 15 })
+  const count = state.count
+
+  function decrementCount() {
+    setState(prevState => {
+      if (count > 1) { 
+        return {...prevState, count: prevState.count -1 }
+        } else {
+          return {...prevState, count: "SOLD OUT"};
+        }
+  })
+  }
+
   return (
   <>
-    <div onClick = {() => props.whenKegClicked(props.id)}>
+    <div >
       <h1>{props.name}</h1>
       <h2>{props.brand}</h2>
       <h3>$ {props.price} / glass | ABV: {props.abv} %</h3>
-      <h3>{props.glassCount} / 397 glasses left in keg</h3>
+      <h3>{ count } / {props.glassCount} glasses left in keg</h3>
+      <div className="kegButtons">
+        <button onClick={decrementCount}>+ Sell Glass</button>
+        <button onClick = {() => props.whenKegClicked(props.id)}>View</button>
+      </div>
     </div>
   </>
   );
